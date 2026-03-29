3b5e4c48ffa5b0a631272bafce5d7135e111cad2
tamerlan-dejavu <tamerlan.dejavu@gmail.com>
2026-03-29 18:43:46 +0500

    I implemented blockHandler and Dodgehandler

--- src/com/narxoz/rpg/chain/ArmorHandler.java
+++ src/com/narxoz/rpg/chain/ArmorHandler.java
@@ -13,7 +13,7 @@ public class ArmorHandler extends DefenseHandler {
     public void handle(int incomingDamage, ArenaFighter target) {
         int absorbed = Math.min(armorValue, incomingDamage); 
         int remainder = incomingDamage - absorbed;           
-        System.out.println("Armor absorbed" + absorbed + " damage. Reminder: " + remainder);
+        System.out.println("Armor absorbed" + absorbed + " damage. Remainder: " + remainder);
         passToNext(remainder, target);
     }
 }
--- src/com/narxoz/rpg/chain/HpHandler.java
+++ src/com/narxoz/rpg/chain/HpHandler.java
@@ -7,6 +7,6 @@ public class HpHandler extends DefenseHandler {
     @Override
     public void handle(int incomingDamage, ArenaFighter target) {
         target.takeDamage(incomingDamage);
-        System.out.println("[HP] " + target.getName() + " takes " + incomingDamage + " damage. HP: " + target.getHealth());
+        System.out.println(target.getName() + " takes " + incomingDamage + " damage. HP: " + target.getHealth());
     }
 }
